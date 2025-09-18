20a7face9959f7e526e364cd4971edf18615818e
sshkim1013 <sshkim1013@naver.com>
2025-09-18 09:24:03 +0900

    0918 first commit

--- js/05-type-conversion/main.js
+++ js/05-type-conversion/main.js
@@ -2,7 +2,7 @@
 // 명시적 형변환: 개발자가 한 것
 // 암시적 형변환: JS 엔진(번역기)이 자동으로 한 것
 
-// 문자열로의 명시적 변환
+// 문자열로의 명시적 형변환
 // String() 함수: 문자열로 변환하는 도구
 // 함수 : 프로그래밍 언어의 특정 기능을 수행하는 도구
 
@@ -36,6 +36,49 @@ console.log(Boolean(0)); // false
 console.log(Boolean(-0)); // false
 console.log(Boolean(99999)); //true
 
+// 숫자 형태가 아닌 데이터를 숫자로 형변환하면?
+// NaN(Not a Number) 발생!
+console.log(Number("1a2b"));
+
 // 문자열 형 변환
 console.log(Boolean("")); // false
-console.log(Boolean("     ")); // true
+console.log(Boolean("     ")); // true, 공백도 문자!
+
+/* 
+[ false 나오는 데이터 ]
+0(숫자), 
+""(빈문자열), 
+null, 
+undefined, 
+NaN */
+
+// 암시적(묵시적) 형변환
+// 불리언 데이터가 아닌 데이터에 대한 논리연산(&&, ||, !)
+
+// 과정: 0 --(암시적)불리언형변환--> false --not연산--> true
+console.log(!0); // true
+console.log(!""); // true
+console.log(!1); // false
+
+// 정확한 논리연산자 작동 방식
+// AND(&&) : 2개의 데이터가 모두 true인 데이터라면 오른쪽 데이터를 생성
+// true && true
+console.log(1 && 2); // 2
+
+// false && true
+console.log(0 && 1); // 0
+
+// false && false
+// 왼쪽 데이터 생성
+console.log(0 && ""); // 0
+
+// OR(||) : 왼쪽 데이터가 true면 왼쪽 데이터 생성,
+// 왼쪽 데이터가 false면, 오른쪽 데이터를 생성
+// true || false
+console.log(1 || 0); // 1
+
+// false || true
+console.log(0 || 10); // 10
+
+// false || false
+console.log("" || 0); // 0
